d86adcc28fbbdca5824bf0a49be3b03d59f3dfbe
Hassan1359h <hahass12na@gmail.com>
2026-09-08 15:22:30 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="warranty-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}))));
