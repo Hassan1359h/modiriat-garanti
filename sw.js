0ce876abcacef2c8b8f555d3b75d93bc333532fb
Hassan1359h <hahass12na@gmail.com>
2026-09-08 15:09:41 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="warranty-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}))));
