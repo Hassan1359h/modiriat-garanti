8c6bfafef0fe621be49cc911688500cb2ce03928
Hassan1359h <hahass12na@gmail.com>
2026-09-09 02:43:03 +0330

    Delete sw-v11.js

--- sw-v11.js
+++
@@ -1,5 +0,0 @@
-const CACHE='rasan-warranty-v11-final';
-const ASSETS=['./app-v11.html','./manifest-v11.json','./icons/rassan-final-192.png','./icons/rassan-final-512.png','./icons/rassan-final-maskable-192.png','./icons/rassan-final-maskable-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
