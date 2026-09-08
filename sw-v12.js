19ed5efb7cc193d17c7d234f70cc092d2c59cdcb
Hassan1359h <hahass12na@gmail.com>
2026-09-09 02:48:28 +0330

    Add files via upload

---
+++ sw-v12.js
@@ -0,0 +1,5 @@
+const CACHE='rasan-warranty-v12-root-icons';
+const ASSETS=['./app-v12.html','./manifest-v12.json','./rassan-final-192.png','./rassan-final-512.png','./rassan-final-maskable-192.png','./rassan-final-maskable-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
